860bf8fc2a3e8d141f35b0737e2135c0a7df717b
zjn-astonishe <627561610@qq.com>
2022-08-03 16:47:56 +0800

    ""

--- update.sh
+++ update.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-while $# -ne 1
-do
+if [ $# -gt 1 ];
+then
   echo "Enter only one parameter"
-done
+  exit
+elif [ $# -lt 1 ];
+then
+  echo "Need one parameter"
+  exit
+fi
+git config --global user.email "627561610@qq.com"
+git config --global user.name "zjn-astonishe"
 git add .
 git commit -m \"$2\"
 git git remote add origin git://github.com/zjn-astonishe/Blog.git
